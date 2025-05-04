0b4c0bcbc1d22628d3a0a1df92151a75ddc9bde8
Sid1298 <siddhant.thakur.1298@gmail.com>
2025-05-04 11:21:58 +0530

    More setup - Clock setup for the game - Added fonts needed to show text - Snake setup with starting position and starting body size - Initializing the first food particle - Starting score set to 0

--- main.py
+++ main.py
@@ -20,3 +20,24 @@ GREY = (30, 30, 30)
 # Screen setup
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Snake Game')
+
+# Intiating clock
+clock = pygame.time.Clock()
+
+# Font for displaying messages
+small_font = pygame.font.SysFont('arial', 15)
+font = pygame.font.SysFont('arial', 25)
+big_font = pygame.font.SysFont('arial', 40)
+
+# Snake setup
+snake_pos = [100, 60]
+snake_body = [[100, 60], [80, 60], [60, 60]]
+direction = 'RIGHT'
+change_to = direction
+
+# Food setup
+food_pos = [random.randrange(0, WIDTH, BLOCK_SIZE), random.randrange(0, HEIGHT, BLOCK_SIZE)]
+food_spawn = True
+
+# Score setup
+score = 0
